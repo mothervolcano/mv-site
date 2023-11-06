e0eb1eeea389f7251a6c33b4aebcab321595b0ec
Eduardo Barbosa <edo@mothervolcano.com>
2023-11-06 16:31:39 +0000

    rebuild layout with new approach

--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,81 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import styles from './page.module.css'
-import {camptonBook, camptonBold} from './styles/fonts';
+import Image from "next/image";
+import styles from "./page.module.css";
+import { camptonBook, camptonBold } from "./styles/fonts";
 
-import PaperStage from './components/paperStage';
-import ProjectCard from './components/ProjectCard';
+import PaperStage from "./components/paperStage";
+import ProjectCard from "./components/ProjectCard";
+import Logo from "./components/logo";
 
-import {init, update, generate} from './demo/main';
+import { init, update, generate } from "./demo/main";
 
 export default function Home() {
-
   const projectList = [
-    
     {
       title: "Polka Folks",
-      descrition: "..."
+      descrition: "...",
     },
 
     {
       title: "Arborator",
-      descrition: "..."
+      descrition: "...",
     },
 
     {
       title: "FASS",
-      descrition: "..."
+      descrition: "...",
     },
 
     {
       title: "Oscill",
-      descrition: "..."
-    }
-
-  ]
+      descrition: "...",
+    },
+  ];
 
   // ---------------------------------------------
   // HANDLERS
 
-  const setStage = ( isPaperLoaded: boolean ) => {
-
-    console.log('Is Paper Loaded? ', isPaperLoaded);
+  const setStage = (isPaperLoaded: boolean) => {
+    console.log("Is Paper Loaded? ", isPaperLoaded);
 
     init();
     generate();
+  };
 
-  }
-
-  const updateStage = ( mousePos: {x:number, y:number}) => {
-
+  const updateStage = (mousePos: { x: number; y: number }) => {
     init();
     update(mousePos);
-    generate()
-
-  }
+    generate();
+  };
 
   return (
-
-    <main className={styles.grid}>
-
-      <div id="cover" className={styles.cover}>
-        <h1 className={camptonBold.className}>Mother Volcano</h1>
-        <h2>Eduardo Barbosa</h2>
-        <PaperStage onPaperLoad={setStage} onMouseClick={updateStage}/>
+    <div className={styles.container}>
+      <div className={styles.cover}>
+        <div className={styles.stage}>
+          <PaperStage
+            onPaperLoad={setStage}
+            onMouseClick={updateStage}
+            onMouseMove={updateStage}
+          />
+        </div>
+        <div className={styles.logo}>
+          <Logo className={styles.logo} />
+        </div>
+        <div className={styles.intro}>
+          One morning, when Gregor Samsa woke from troubled dreams, he found
+          himself transformed in his bed into a horrible vermin. He lay on his
+          armour-like back, and if he lifted his head a little he could see his
+          brown belly, slightly domed and divided by arches into stiff sections.
+        </div>
       </div>
 
-        
-      <div id="gallery" className={`${styles.gallery} ${camptonBold.className}`}>
-
-        { projectList.map( ( n ) => { return <ProjectCard key={n.title} className={styles.projectCard} title={n.title}/> }) }
-
+      <div className={styles.projects}>
+        <div className={styles.project}>Project #1</div>
+        <div className={styles.project}>Project #2</div>
+        <div className={styles.project}>Project #3</div>
+        <div className={styles.project}>Project #4</div>
       </div>
-
-    </main>
-  )
+    </div>
+  );
 }
