348122f34972e69c5e87fd94ea5c8d39d5375bcc
Eduardo Barbosa <edo@mothervolcano.com>
2023-11-28 12:48:38 +0000

    add thumbnail images to projects

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,4 +1,12 @@
-import { camptonLight, camptonBook, camptonMedium, camptonSemiBold, camptonBold } from "../styles/fonts";
+import {
+	camptonLight,
+	camptonBook,
+	camptonMedium,
+	camptonSemiBold,
+	camptonBold,
+} from "../styles/fonts";
+
+import Image from "next/image";
 
 import {
 	ActionIcon,
@@ -9,9 +17,13 @@ import {
 	Flex,
 	Text,
 	Title,
-	rem
+	rem,
 } from "@mantine/core";
-import { IconArrowUpRight, IconChevronUpRight, IconEye } from "@tabler/icons-react";
+import {
+	IconArrowUpRight,
+	IconChevronUpRight,
+	IconEye,
+} from "@tabler/icons-react";
 
 export default function ProjectCard(props: any) {
 	const { image, title, contrast, description, link } = props;
@@ -26,34 +38,35 @@ export default function ProjectCard(props: any) {
 		fontFamily: camptonMedium.style.fontFamily,
 		fontSize: rem(30),
 		marginLeft: rem(10),
-		marginTop: rem(5)
+		marginTop: rem(5),
 	};
 
 	const bodyTypo = {
-		fontFamily: camptonBook.style.fontFamily
-	}
+		fontFamily: camptonBook.style.fontFamily,
+	};
 
 	return (
 		<div
 			style={{
 				display: "relative",
-				backgroundImage: `url(${image})`,
-				backgroundSize: "cover",
+				// backgroundImage: `url(${image})`,
+				// backgroundSize: "cover",
 				borderRight: "1px solid black",
 				borderBottom: "1px solid black",
 			}}
 		>
-			<AspectRatio ratio={1/1}>
+			<AspectRatio ratio={1 / 1}>
+				<Image
+					alt="Polka Folks project thumbnail image"
+					src={image}
+				/>
 				<Flex
 					direction="column"
 					h="100%"
 					justify="space-between"
 					align="flex-start"
 				>
-					<Title
-						style={titleTypo}
-						order={2}
-					>
+					<Title style={titleTypo} order={2}>
 						{title}
 					</Title>
 					<Flex direction="column" align="flex-end" p={0}>
@@ -67,13 +80,17 @@ export default function ProjectCard(props: any) {
 						</ActionIcon>
 						<Container bg="white" p={0}>
 							<Divider size="xs" color="black" />
-							<Text
-								style={bodyTypo}
-								size="sm"
-								lh='xs'
-								p={10}
-							>{description}</Text>
-							<Button ml={10} variant="transparent" size="compact-xs" leftSection={<IconEye stroke={1.5}/>}>See on GitHub</Button>
+							<Text style={bodyTypo} size="sm" lh="xs" p={10}>
+								{description}
+							</Text>
+							<Button
+								ml={10}
+								variant="transparent"
+								size="compact-xs"
+								leftSection={<IconEye stroke={1.5} />}
+							>
+								See on GitHub
+							</Button>
 						</Container>
 					</Flex>
 				</Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,13 @@ import {
   rem
 } from "@mantine/core";
 
-import { init, update, generate } from "./demo/main";
+import thumbPolkaFolks from "../public/img/project_thumb_polka.png";
+import thumbFass from "../public/img/project_thumb_fass.png";
+import thumbFassPlugin from "../public/img/project_thumb_fass_plugin.png";
+import thumbArborator from "../public/img/project_thumb_arborator.png";
+import thumbPolystar from "../public/img/project_thumb_polystar.png";
+
+import { init, update, generate } from "./demo-two/main";
 import { useState } from "react";
 
 const projectList = [
@@ -92,31 +98,31 @@ export default function Home() {
         <ProjectCard
           title={"Polka Folks"}
           contrast="LIGHT"
-          image="https://images.unsplash.com/photo-1673913816987-e7b0fb6ff6d2?q=80&w=4000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image={thumbPolkaFolks}
           description={`One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.`}
         />
         <ProjectCard
           title={"FASS"}
-          contrast="LIGHT"
-          image="https://images.unsplash.com/photo-1698633811105-a88dc770852c?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          description={`Drops of rain could be heard hitting the pane, which made him feel quite sad. "How about if I sleep a little bit longer and forget all this nonsense", he thought, but that was something he was unable to do because he was used to sleeping on his right, and in his present state couldn't get into that position.`}
+          contrast="DARK"
+          image={thumbFass}
+          description={`"How about if I sleep a little bit longer and forget all this nonsense", he thought, but that was something he was unable to do because he was used to sleeping on his right, and in his present state couldn't get into that position.`}
         />
         <ProjectCard 
           title={"Arborator"}
-          contrast="DARK"
-          image="https://plus.unsplash.com/premium_photo-1676117274720-545b12f547fb?q=80&w=4374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          contrast="LIGHT"
+          image={thumbArborator}
           description={`The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me? " `}
         />
         <ProjectCard 
           title={"Polystar"}
-          contrast="DARK"
-          image="https://images.unsplash.com/photo-1699355484587-b5018a07b73e?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          contrast="LIGHT"
+          image={thumbPolystar}
           description={`It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.`} 
         />
         <ProjectCard 
           title={"FASS Plugin"}
-          contrast="DARK"
-          image="https://images.unsplash.com/photo-1697606378971-4a3bb7b64bd0?q=80&w=4384&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          contrast="LIGHT"
+          image={thumbFassPlugin}
           description={`You've got to get enough sleep. Other travelling salesmen live a life of luxury. For instance, whenever I go back to the guest house during the morning to copy out the contract, these gentlemen are always still sitting there eating their breakfasts. `}
         />
       </div>
