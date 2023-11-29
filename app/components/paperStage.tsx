a3943a68ca79b606d221a3d671211b31ed3d6514
Eduardo Barbosa <edo@mothervolcano.com>
2023-11-29 16:24:31 +0000

    fix #4 + page loading glitch

--- app/components/paperStage.tsx
+++ app/components/paperStage.tsx
@@ -2,35 +2,19 @@
 
 declare const paper: any;
 
-import styles from "../page.module.css";
-
-import { useRef, useEffect, useState } from "react";
-
-// import { useViewportSize } from "@mantine/hooks";
 import useMousePosition from "../hooks/useMousePosition";
-
-// const onViewportChange = (width: number, height: number) => {
-// 	console.log(`! viewport changed: width: ${width} height: ${height}`);
-// };
 
 const PaperStage = ({ canvasRef, width, height, onMouseMove}: any) => {
 	// ...
 	const [mousePos, setMousePos] = useMousePosition();
-	// const { width, height } = useViewportSize();
 
 	if (canvasRef.current) {
 		onMouseMove(mousePos);
 	}
 
-	// console.log("MOUNTING PAPERSTAGE!");
-
-	// const canvasWidth = width ? `${width}px` : "100%";
-
-	// onViewportChange(width, height);
-
 	return (
 		<canvas
-			style={{ backgroundColor: "green", position: "relative", width: `${width}px`, height: `${height}px` }}
+			style={{ zIndex: "1", position: "relative", width: `${width}px`, height: `${height}px` }}
 			ref={canvasRef}
 			// onClick={(e) => {
 			// 	setMousePos(e as unknown as MouseEvent);
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import { useEffect, useRef, useState } from "react";
 // import Image from "next/image";
 import styles from "./page.module.css";
 import { useViewportSize } from "@mantine/hooks";
-import useMousePosition from "./hooks/useMousePosition";
 
 import PaperStage from "./components/paperStage";
 import ProjectCard from "./components/ProjectCard";
@@ -83,73 +82,76 @@ export default function Home() {
     generate();
   };
 
+  const coverAreaWidth = `${(width * 1) / 3}px`;
+  const projectsAreaWidth = `${(width * 2) / 3}px`;
+
   return (
-    <div className={styles.container}>
-      <div style={{ width: `${width * 1/3}px`, height: `${height}px` }} className={styles.cover}>
-        <div className={styles.stage}>
-          <PaperStage
-            canvasRef={canvasRef}
-            width={width * 1/3}
-            height={height}
-            onMouseMove={updateStage}
-          />
-        </div>
-        <Script
-          src="../lib/paper/paper-core.js"
-          onReady={() => {
-            paper.install(window);
-            paper.setup(canvasRef.current);
-            setStage(true);
-          }}
-        />
-        <div className={styles.logo}>
-          <Logo className={styles.logo} />
-        </div>
-
-        <Flex direction="row" justify="flex-end" align="flex-end" h="100%">
-          <Container pl="25%" pb={rem(50)}>
-            <Title order={2}>Introduction Title</Title>
-            <div className={styles.intro}>
-              One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a
-              horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown
-              belly, slightly domed and divided by arches into stiff sections.
+    <>
+      {width ? (
+        <div className={styles.container}>
+          <div style={{ width: coverAreaWidth, height: "100%" }} className={styles.cover}>
+            <div className={styles.stage}>
+              <PaperStage canvasRef={canvasRef} width={(width * 1) / 3} height={height} onMouseMove={updateStage} />
             </div>
-          </Container>
-        </Flex>
-      </div>
+            <Script
+              src="../lib/paper/paper-core.js"
+              onReady={() => {
+                paper.install(window);
+                paper.setup(canvasRef.current);
+                setStage(true);
+              }}
+            />
+            <div className={styles.logo}>
+              <Logo className={styles.logo} />
+            </div>
+            <Flex style={{ zIndex: "100" }} direction="row" justify="flex-end" align="flex-end" h="100%">
+              <Container pl="25%" pb={rem(50)}>
+                <Title order={2}>Introduction Title</Title>
+                <div className={styles.intro}>
+                  One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into
+                  a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his
+                  brown belly, slightly domed and divided by arches into stiff sections.
+                </div>
+              </Container>
+            </Flex>
+          </div>
 
-      <div style={{ width: `${width * 2/3}px`, height: `${height}px` }} className={styles.projects}>
-        <ProjectCard
-          title={"Polka Folks"}
-          contrast="LIGHT"
-          image={thumbPolkaFolks}
-          description={`One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.`}
-        />
-        <ProjectCard
-          title={"FASS"}
-          contrast="DARK"
-          image={thumbFass}
-          description={`"How about if I sleep a little bit longer and forget all this nonsense", he thought, but that was something he was unable to do because he was used to sleeping on his right, and in his present state couldn't get into that position.`}
-        />
-        <ProjectCard
-          title={"Arborator"}
-          contrast="LIGHT"
-          image={thumbArborator}
-          description={`The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me? " `}
-        />
-        <ProjectCard
-          title={"Polystar"}
-          contrast="LIGHT"
-          image={thumbPolystar}
-          description={`It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.`}
-        />
-        <ProjectCard
-          title={"FASS Plugin"}
-          contrast="LIGHT"
-          image={thumbFassPlugin}
-          description={`You've got to get enough sleep. Other travelling salesmen live a life of luxury. For instance, whenever I go back to the guest house during the morning to copy out the contract, these gentlemen are always still sitting there eating their breakfasts. `}
-        />
-      </div>
-    </div>
+          <div style={{ width: projectsAreaWidth, height: "100%" }} className={styles.projects}>
+            <ProjectCard
+              title={"Polka Folks"}
+              contrast="LIGHT"
+              image={thumbPolkaFolks}
+              description={`One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.`}
+            />
+            <ProjectCard
+              title={"FASS"}
+              contrast="DARK"
+              image={thumbFass}
+              description={`"How about if I sleep a little bit longer and forget all this nonsense", he thought, but that was something he was unable to do because he was used to sleeping on his right, and in his present state couldn't get into that position.`}
+            />
+            <ProjectCard
+              title={"Arborator"}
+              contrast="LIGHT"
+              image={thumbArborator}
+              description={`The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me? " `}
+            />
+            <ProjectCard
+              title={"Polystar"}
+              contrast="LIGHT"
+              image={thumbPolystar}
+              description={`It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.`}
+            />
+            <ProjectCard
+              title={"FASS Plugin"}
+              contrast="LIGHT"
+              image={thumbFassPlugin}
+              description={`You've got to get enough sleep. Other travelling salesmen live a life of luxury. For instance, whenever I go back to the guest house during the morning to copy out the contract, these gentlemen are always still sitting there eating their breakfasts. `}
+            />
+          </div>
+        </div>
+      ) : (
+        <div>loading...</div>
+      )}
+    </>
   );
 }
