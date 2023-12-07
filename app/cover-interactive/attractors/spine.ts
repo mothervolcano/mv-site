4deb67d0e991197041a10ee869f3eee73423dd60
Eduardo Barbosa <edo@mothervolcano.com>
2023-12-07 22:35:36 +0000

    refactor to integrate with changes in the base classes

--- app/cover-interactive/attractors/spine.ts
+++ app/cover-interactive/attractors/spine.ts
@@ -1,171 +1,101 @@
-import { Point, TopoPath, Group, Circle } from "../../lib/topo/drawing/paperjs";
+import AttractorTopo from "@/app/lib/topo/core/attractorTopo";
+import HyperPoint from "@/app/lib/topo/core/hyperPoint";
+import { TopoPath } from "@/app/lib/topo/drawing/paperjs";
+import { IHyperPoint, IPath, IPoint, TopoLocationData } from "@/app/lib/topo/types";
 
-import { PathLocationData, IHyperPoint, PointLike, IPath, IGroup } from "../../lib/topo/types";
-import { convertToHyperPoint, convertToPoint } from "../../lib/topo/utils/converters";
 
-import AttractorObject from "../../lib/topo/core/attractorObject";
-import HyperPoint from "../../lib/topo/core/hyperPoint";
+class Spine extends AttractorTopo {
 
-function isHyperPoint(value: any): value is IHyperPoint {
-	return value != null && typeof value === "object" && "spin" in value && "polarity" in value;
-}
+	constructor(length: number, anchor?: IHyperPoint ) {
 
-class Spine extends AttractorObject {
-	private DEBUG_MODE: boolean = false;
+		const topoPath: IPath = new TopoPath()
 
-	private _A: any;
-	private _B: any;
+		// console.log('SPINE: ', topoPath)
 
-	// protected _path: any
+		super(topoPath, anchor);
+		this.setLength(length);
+		this.draw();
+	}
 
-	// private _debugPath1: IPath;
-	// private _debugPath2: IPath;
-	// private _debugPath3: IPath;
-	// private _debugPath4: IPath;
-	// private _arrow: IGroup;
+	draw() {
 
-	static project(positionData: PointLike | IHyperPoint | [IHyperPoint, IHyperPoint], length: number | null = null) : IPath {
-		let projectedPath: IPath;
+		if (this.anchor) {
 
-		// --------------------------------------------------------------
-		// Create Spine from 2 points
+			this.topo.reset();
+			const A: IPoint = this.anchor.point.subtract([this.length/2, 0]);
+			const B: IPoint = this.anchor.point.add([this.length/2, 0]);
+			this.topo.add(A,B);
 
-		// TODO: validate positionData
+			this.topo.visibility = false;
 
-		if (
-			Array.isArray(positionData) &&
-			positionData.length === 2 &&
-			isHyperPoint(positionData[0]) &&
-			isHyperPoint(positionData[1])
-		) {
-			const hyperPoints: [IHyperPoint, IHyperPoint] = positionData as [IHyperPoint, IHyperPoint];
-
-			console.log(positionData);
-
-			projectedPath = new TopoPath();
-			projectedPath.add(...hyperPoints);
-
-			projectedPath.fullySelected = true;
-
-			return projectedPath;
-
-			// --------------------------------------------------------------
-			// Create Spine from 1 point
-		} else if (length) {
-			const C = convertToPoint(positionData);
-
-			const vA = new Point({ angle: 180, length: length / 2 });
-			const vB = new Point({ angle: 0, length: length / 2 });
-
-			const A = new Point(C.add(vA));
-			const B = new Point(C.add(vB));
-
-			// projectedPath = new Path({
-			// 	segments: [A, B],
-			// });
-
-			projectedPath = new TopoPath();
-			projectedPath.add( A, B );
-
-			// projectedPath.fullySelected = true;
-
-			return projectedPath;
-		} else {
-			return new TopoPath({ segments: [[0, 0]] });
+			// this.topo.strokeColor = new paper.Color("blue");
 		}
 	}
 
-	constructor(
-		length: number,
-		positionData: PointLike | IHyperPoint | [IHyperPoint, IHyperPoint] = new HyperPoint(new Point(0, 0)),
-	) {
-		const path = Spine.project(positionData, length);
+	configureAttractor() {
 
-		const pos = convertToHyperPoint(path.getPointAt(path.length / 2))
+		if (this.field) {
 
-		super(pos);
-
-		// -------------------------------
-		// DEBUG
-
-		// this._debugPath1 = new TopoPath();
-		// this._debugPath2 = new TopoPath();
-		// this._debugPath3 = new TopoPath();
-		// this._debugPath4 = new TopoPath();
-
-		// this._arrow = new Group();
-
-		// -------------------------------
-
-		this._A = new HyperPoint(path.firstPoint.point, path.firstPoint.handleIn, path.firstPoint.handleOut);
-		this._B = new HyperPoint(path.lastPoint.point, path.lastPoint.handleIn, path.lastPoint.handleOut);
-
-		this.render();
-
-		// super.render();
-		// super.render(new Group([path, this._arrow]));
-
-		return this;
+			this.setOrientationDeterminator(this.field.determineOrientation)
+			this.setSpinDeterminator(this.field.determineSpin)
+			this.setPolarityDeterminator(this.field.determinePolarity)
+		}
 	}
 
-	protected render() {
-		if (this.isRendered) {
-			this.content.remove();
-			this.isRendered = false;
-		}
+	adjustToPosition() {
 
-		this._path = new TopoPath();
-		this._path.add(this._A, this._B);
-		this._path.visibility = false;
+		console.log('5 adjusting spine: ', this.anchor.position)
 
-		if (this.DEBUG_MODE) {
-			this._path.strokeColor = new paper.Color("#02B7FD");
-			this._path.visibility = true;
-			// this.addOrientationArrow(this._path);
-		}
-
-		super.render(this._path)
-		
-		// super.render(new Group([this._path, this._arrow]));
-
-		// this._path.fullySelected = true;
-
-	}
-
-	public adjustRotationToPosition(anchor: any, isPositive: Function, isNegative: Function) {
-		if (isPositive(anchor.position)) {
-			this.axisAngle = 0;
-		} else if (isNegative(anchor.position)) {
-			this.axisAngle = 180;
+		if (this.determineOrientation(this.anchor.position)) {
+			this.setAxisAngle(0);
+		} else if (!this.determineOrientation(this.anchor.position)) {
+			this.setAxisAngle(180);
 		} else {
 			throw new Error("POSSIBLY TRYING TO ANCHOR OUTSIDE OF FIELDs BOUNDS");
 		}
 	}
 
-	public adjustToOrientation(anchor: any, isPositive: Function, isNegative: Function) {
-		if (isPositive(anchor.position)) {
-			this.orientation = 1;
-		} else if (isNegative(anchor.position)) {
-			this.orientation = -1;
+	adjustToSpin() {
+		if (this.determineSpin(this.anchor.position)) {
+			this.setSpin(1);
+		} else if (!this.determineSpin(this.anchor.position)) {
+			this.setSpin(-1);
 		} else {
 			throw new Error("POSSIBLY TRYING TO PLACE THE ATTRACTOR OUTSIDE OF FIELDs BOUNDS");
 		}
 	}
 
-	public adjustToPolarity(anchor: any) {
+	adjustToPolarity() {
 		// TODO
-
-		this.polarity = 1;
+		this.setPolarity(1);
 	}
 
-	protected getPathLocationDataAt(at: number): PathLocationData {
+	// at is provided by attractors that have paths that are non-linear ie. the input location doesn't match the mapped location.
+	createAnchor({ point, tangent, normal, curveLength, pathLength, at }: TopoLocationData): IHyperPoint {
+		const factor = [0, 0.25, 0.5, 0.75].includes(at) ? 1 / 3 : curveLength / pathLength;
 
-		if ( !this._path ) {
+		const hIn = tangent.multiply(curveLength * factor).multiply(-1);
+		const hOut = tangent.multiply(curveLength * factor);
 
-			throw new Error(`ERROR @Spine.getPathLocationDataAt(${at}) ! Path is missing`)
+		const anchor = new HyperPoint(point, hIn, hOut);
+
+		anchor.position = at;
+		anchor.tangent = tangent.multiply(this.spin); // HACK: because the path is flipped using scale() the vectors need to be inverted
+		anchor.normal = normal.multiply(this.spin);
+		anchor.spin = this.spin;
+		anchor.polarity = this.polarity;
+
+		return anchor;
+	}
+
+	getTopoLocationAt(at: number): TopoLocationData {
+
+		if ( !this.topo ) {
+
+			throw new Error(`ERROR @Spine.getTopoLocationAt(${at}) ! Topo path is missing`)
 		}
 
-		const loc = this._path.getLocationAt(this._path.length * at);
+		const loc = this.topo.getLocationAt(this.topo.length * at);
 
 		return {
 			point: loc.point,
@@ -176,54 +106,7 @@ class Spine extends AttractorObject {
 			at: at,
 		};
 	}
-
-	// private addOrientationArrow(path: IPath) {
-	// 	this._debugPath1.remove();
-	// 	this._debugPath2.remove();
-	// 	this._debugPath3.remove();
-	// 	this._debugPath4.remove();
-
-	// 	this._arrow.remove();
-
-	// 	this._arrow = new Group();
-
-	// 	let midPoint = path.getPointAt(path.length / 2);
-
-	// 	this._debugPath1 = new TopoPath({ segments: [midPoint, path.segments[1]], strokeColor: "#70D9FF" });
-
-	// 	let _A = this._debugPath1.lastPoint.point.subtract(this._debugPath1.lastPoint.tangent.multiply(5));
-	// 	let _Ar = _A.rotate(30, this._debugPath1.lastPoint.point);
-
-	// 	let _B = this._debugPath1.lastPoint.point.subtract(this._debugPath1.lastPoint.tangent.multiply(5));
-	// 	let _Br = _B.rotate(-40, this._debugPath1.lastPoint.point);
-
-	// 	this._debugPath2 = new TopoPath({
-	// 		segments: [this._debugPath1.lastPoint.point, _Ar],
-	// 		strokeColor: "#70D9FF",
-	// 	});
-
-	// 	this._debugPath3 = new TopoPath({
-	// 		segments: [this._debugPath1.lastPoint.point, _Br],
-	// 		strokeColor: "#70D9FF",
-	// 	});
-
-	// 	this._debugPath4 = new Circle({ center: midPoint, radius: 2, fillColor: "#70D9FF" });
-
-	// 	this._arrow.addChild(this._debugPath1);
-	// 	this._arrow.addChild(this._debugPath2);
-	// 	this._arrow.addChild(this._debugPath3);
-	// 	this._arrow.addChild(this._debugPath4);
-	// }
-
-	public clearGuides() {
-		// TODO
-	}
-
-	// public anchorAt( location: any, along: string = 'RAY' ) {
-
-	// 	super.anchorAt( location, along );
-	// 	// this.rotate(180);
-	// }
 }
 
 export default Spine;
+
